be5f8a827ef47bfebd3ab6fd01526b11881b5744
Rudy Matela <rudy@matela.com.br>
2017-08-14 11:09:50 +0100

    Def: add Show instances

--- gen/src/Def.hs
+++ gen/src/Def.hs
@@ -6,18 +6,23 @@ import qualified Data.Map      as Map
 
 -- | A sum of products.
 data Sum  = Sum [Prod]
+  deriving Show
 
 -- | A product is a list of variables.
 data Prod = Prod [Var]
+  deriving Show
 
 -- | A variable is identified by an Int index.  It also stores the
 --   name of the data type originally corresponding to the variable.
 data Var  = Var Int String
+  deriving Show
 
 -- | A generic definition of a particular data type as a
 --   sum-of-products.
 data Def = Def DataType Sum
+  deriving Show
 
 -- | A group of definitions is a mapping from names to definitions
 --   paired with their corresponding variable index.  (???)
 newtype Defs = Defs (Map String (Def, Int))
+  deriving Show
